56f15c3f2d47d10d0fa9256c74c1643a63472375
trip1ezer0 <43763794+trip1ezer0@users.noreply.github.com>
2020-10-13 19:14:48 -0400

    Fixed Jump, changed camera size, added sprite to ramsey, fixed movement on hill, added camera follow

---
+++ Ramsey's Revenge/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LayerMask platformsLayerMask;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    public float moveSpeed = 80f;
+    public float jumpVelocity = 200f;
+    private float movement;
+    private float gravity;
+    public Sprite left;
+    public Sprite right;
+
+    private void Start()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        gravity = rigidbody2d.gravityScale;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Move Code
+        movement = Input.GetAxis("Horizontal");
+
+        if (IsGrounded())
+        {
+            rigidbody2d.gravityScale = 0f;
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }
+        else
+        {
+            rigidbody2d.gravityScale = gravity;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2d.velocity = new Vector2(Vector2.left.x * moveSpeed, rigidbody2d.velocity.y);
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2d.velocity = new Vector2(Vector2.right.x * moveSpeed, rigidbody2d.velocity.y);
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = right;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rigidbody2d.velocity = new Vector2(0f, rigidbody2d.velocity.y);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rigidbody2d.velocity = new Vector2(0f, rigidbody2d.velocity.y);
+        }
+
+        // Jump Code
+        if (IsGrounded() && Input.GetKey(KeyCode.W))
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, Vector2.up.y * jumpVelocity);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .5f, platformsLayerMask);
+        return raycastHit2d.collider != null;
+    }
+}

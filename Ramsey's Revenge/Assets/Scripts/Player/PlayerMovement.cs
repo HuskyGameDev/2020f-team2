b490f69c8f164baac82354c413b3900bd0304a9d
trip1ezer0 <43763794+trip1ezer0@users.noreply.github.com>
2020-10-20 20:31:25 -0400

    Moving right, left, and jumping uses the sprites to make an animation

--- Ramsey's Revenge/Assets/Scripts/Player/PlayerMovement.cs
+++ Ramsey's Revenge/Assets/Scripts/Player/PlayerMovement.cs
@@ -40,34 +40,42 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             rigidbody2d.velocity = new Vector2(Vector2.left.x * moveSpeed, rigidbody2d.velocity.y);
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = left;
+            this.gameObject.GetComponent<Animator>().enabled = true;
+            this.gameObject.GetComponent<Animator>().Play("MoveLeft");
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rigidbody2d.velocity = new Vector2(Vector2.right.x * moveSpeed, rigidbody2d.velocity.y);
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = right;
+            this.gameObject.GetComponent<Animator>().enabled = true;
+            this.gameObject.GetComponent<Animator>().Play("MoveRight");
         }
 
         if (Input.GetKeyUp(KeyCode.A))
         {
             rigidbody2d.velocity = new Vector2(0f, rigidbody2d.velocity.y);
+            this.gameObject.GetComponent<Animator>().enabled = false;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = left;
         }
 
         if (Input.GetKeyUp(KeyCode.D))
         {
             rigidbody2d.velocity = new Vector2(0f, rigidbody2d.velocity.y);
+            this.gameObject.GetComponent<Animator>().enabled = false;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = right;
         }
 
         // Jump Code
         if (IsGrounded() && Input.GetKey(KeyCode.W))
         {
             rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, Vector2.up.y * jumpVelocity);
+            this.gameObject.GetComponent<Transform>().localRotation = new Quaternion(0f, 0f, 0f, 0f);
+            this.gameObject.GetComponent<Animator>().Play("JumpLeft");
         }
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .5f, platformsLayerMask);
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
         return raycastHit2d.collider != null;
     }
 }

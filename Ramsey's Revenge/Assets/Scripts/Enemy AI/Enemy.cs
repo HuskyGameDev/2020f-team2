7016b691c305ec45485959c8d934040918eecaf3
Ethan Wright <ethanw@mtu.edu>
2020-12-01 18:24:06 -0500

    Revert "AI Attack"
    This reverts commit ad46b4c1eee6007318e6c850f1da04cfdc83afe1.

--- Ramsey's Revenge/Assets/Scripts/Enemy AI/Enemy.cs
+++ Ramsey's Revenge/Assets/Scripts/Enemy AI/Enemy.cs
@@ -5,44 +5,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private int health = 100;
-    private int damage = 25;
-    private GameObject player;
-
-    private void Start()
+    private void InitializeStateMachine()
     {
-        //player = FindObjectOfType<PlayerAttacking>();
-    }
+        var states = new Dictionary<Type, BaseState>() {
+            { typeof(WanderState), new WanderState(this) }//,
+            //{ typeof(ChaseState), new ChaseState(this) },
+            //{ typeof(AttackState), new AttackState(this) }
+        };
 
-    private void Update()
-    {
-        if (player.transform.position.magnitude < 1)
-        {
-            StartCoroutine(attackPlayer());
-        }
-    }
 
-    private IEnumerator attackPlayer()
-    {
-        //will play the animation and damage player
-        playAttackAnimation();
-        yield return new WaitForSeconds(3);
-        damagePlayer();
-        yield return new WaitForSeconds(2);
-    }
-
-    private void damagePlayer()
-    {
-        //player.GetComponent<PlayerAttacking>().takeDamage(damage);
-    }
-
-    public void takeDamage(int damageTaken)
-    {
-        health -= damageTaken;
-    }
-
-    private void playAttackAnimation()
-    {
-        this.gameObject.GetComponent<Animator>().Play("Attack");
     }
 }
---
+++ Ramsey's Revenge/Assets/Scripts/Enemy AI/StateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private Dictionary<Type, BaseState> _availabeStates;
+
+    public void SetStates(Dictionary<Type, BaseState> states)
+    {
+        _availabeStates = states;
+    }
+}

e841a4f016a4d850674987cd1509fe8d9576ca6b
n-wiszowaty <43485233+n-wiszowaty@users.noreply.github.com>
2020-12-01 17:28:18 -0600

    PlayerHealth now has code

--- Ramsey's Revenge/Assets/Scripts/Player/PlayerHealth.cs
+++ Ramsey's Revenge/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    public int health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100; // default health
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //checks for death
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //can be called by enemies
+    public void playerTakesDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }

92042c7da3b03b4c22589a6344ed7e4d00d99e79
Ethan Wright <ethanw@mtu.edu>
2020-12-09 18:40:44 -0500

    Kill Box done

---
+++ Ramsey's Revenge/Assets/Scripts/KillBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.position = new Vector3(-155, 31, 0);
+        mainCamera.transform.position = new Vector3(-90, 103, -1);
+    }
+}

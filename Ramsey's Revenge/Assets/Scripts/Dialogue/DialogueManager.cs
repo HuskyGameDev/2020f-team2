b85e8b6e4c70ff67f091a68c3d57c1b716a682ea
n-wiszowaty <43485233+n-wiszowaty@users.noreply.github.com>
2021-02-02 18:18:00 -0600

    Added Dialogue Scripts, they're not fully implemented yet

---
+++ Ramsey's Revenge/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogeManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialoage)
+    {
+        Debug.Log("Starting conversation with" + dialoage.name);
+    }
+}
---
+++ Ramsey's Revenge/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            FindObjectOfType<DialogeManager>().StartDialogue(dialogue);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            FindObjectOfType<DialogeManager>().StartDialogue(dialogue);
+        }
+    }
+}

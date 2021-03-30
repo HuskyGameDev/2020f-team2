5053d9d83190d732a6fd6836cedd009fb97961f0
Rachel Hartrick <rehartri@mtu.edu>
2021-03-30 19:29:59 -0400

    Boss Dude
    He shoot fire ball, he walk back and forth, he do a spin, he do everythin'

--- Ramsey's Revenge/Assets/Scripts/Enemy AI/BossAlien.cs
+++ Ramsey's Revenge/Assets/Scripts/Enemy AI/BossAlien.cs
@@ -4,31 +4,82 @@ using UnityEngine;
 
 public class BossAlien : MonoBehaviour
 {
-    public Transform mothership;
     public int speed;
+    public int health;
+    public Transform mothership;
+    public GameObject player;
+    public GameObject danger;
 
     private bool movingRight;
+    private bool waiting;
 
     // Start is called before the first frame update
     void Start()
     {
         movingRight = true;
+        waiting = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-        if (Vector2.Distance(transform.position, mothership.position) < 10f)
+        transform.Translate(Vector2.right * speed * Time.deltaTime);//Get the enemy moving
+        //Debug.Log(Vector2.Distance(transform.position, player.transform.position));
+        if (Vector2.Distance(transform.position, player.transform.position) < 10f)//Chase the player they got too close
         {
-            if(movingRight == true)
+            if (player.transform.position.x > transform.position.x)//Chase the player to the right
             {
-                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
             }
-            else
+            else//Chase the player to the left
             {
-                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
             }
         }
+        else
+        {
+            if (Vector2.Distance(transform.position, mothership.position) > 10f)//Keeps enemy in a certain range
+            {
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+            }
+            if (Vector2.Distance(transform.position, mothership.position) > 10.1f)//Chased player too far? No problem, enemy will be teleported back home
+            {
+                transform.position = mothership.position;
+            }
+        }
+        if (Vector2.Distance(transform.position, player.transform.position) < 10f)
+        {
+            Vector2 start = new Vector2(transform.position.x, transform.position.y + 1);
+            //Instantiate(danger, start, Quaternion.identity);
+            if (waiting)
+            {
+                StartCoroutine(SpitAttack(0.5f));
+            }
+        }
+    }
+    public void TakeDamage(int damageTaken)//Allows Ramsey to kill the enemy
+    {
+        health -= damageTaken;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+    private IEnumerator SpitAttack(float time)
+    {
+        waiting = false;
+        yield return new WaitForSeconds(time);
+        waiting = true;
+        Vector2 start = new Vector2(transform.position.x, transform.position.y + 1);
+        Instantiate(danger, start, Quaternion.identity);
     }
 }
---
+++ Ramsey's Revenge/Assets/Scripts/Enemy AI/Fire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public int time;
+    public float speed;
+
+    private Transform target;
+    private GameObject player;
+
+    private int count;
+    private int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        count = 0;
+        damage = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            if (target.position.x < transform.position.x)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-50, target.position.y * 3);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(50, target.position.y * 3);
+            }
+
+        }
+        count++;
+        if (count == time)//Gets rid of the spit after a certain amount of time
+        {
+            Destroy(GetComponent<Collider2D>().gameObject);
+        }
+    }
+    //private IEnumerator AttackPlayer()
+    //{
+    //    Debug.Log(player);
+    //    player.GetComponent<PlayerHealth>().SendMessage("PlayerTakesDamage", damage);
+    //    yield return new WaitForSeconds(3f);
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<PlayerHealth>().SendMessage("PlayerTakesDamage", damage);
+        }
+        Destroy(GetComponent<Collision2D>().gameObject);
+    }
+}

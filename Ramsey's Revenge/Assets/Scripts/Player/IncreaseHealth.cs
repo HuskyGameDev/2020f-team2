7b91367b50dcd80b25630032f842204276732b6d
trip1ezer0 <43763794+trip1ezer0@users.noreply.github.com>
2021-04-15 18:15:01 -0400

    Adjusted big piggy fight. added walls, fixed platforms, added hearts to increase max health

---
+++ Ramsey's Revenge/Assets/Scripts/Player/IncreaseHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseHealth : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerHealth>().numOfHearts += 1;
+            int hearts = collision.GetComponent<PlayerHealth>().numOfHearts;
+            collision.GetComponent<PlayerHealth>().health = hearts * 25;
+            Destroy(gameObject);
+        }
+    }
+}
